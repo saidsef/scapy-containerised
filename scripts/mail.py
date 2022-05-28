273c2ad7dcd5e3ce5d9732623ef9b45467a7e124
Said Sef <saidsef@gmail.com>
2022-05-28 11:30:33 +0000

    Update scripts to include def docs

--- scripts/mail.py
+++ scripts/mail.py
@@ -3,11 +3,12 @@
 from scapy.all import *
 
 def mail(packet):
-    if packet.haslayer(TCP) and packet[TCP].payload:
-        mail = str(packet[TCP].payload)
+  '''Takes raw packets and prints destination IP and payload'''
+  if packet.haslayer(TCP) and packet[TCP].payload:
+    mail = str(packet[TCP].payload)
 
-        if "user" in mail.lower() or "pass" in mail.lower():
-            print "[*] Server: %s" % packet[IP].dst
-            print "[*] %s" % packet[TCP].payload
+    if "user" in mail.lower() or "pass" in mail.lower():
+      print("[*] Server: {}".fotmat(packet[IP].dst))
+      print("[*] {}".format(packet[TCP].payload))
 
 sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=mail, store=0)
--- scripts/traffic.py
+++ scripts/traffic.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 
 import json
+import logging
 from scapy.all import *
 from scapy.utils import *
 from scapy.layers import http
@@ -8,12 +9,14 @@ from scapy.layers import http
 load_layer('tls')
 
 def sniffer(packet):
-    if packet.haslayer(Raw):
-        r = packet.getlayer(Raw).load
-        try:
-            p = json.loads(r)
-            print(p)
-        except:
-            pass
+  '''Takes raw packets and prints packet as string'''
+  if packet.haslayer(Raw):
+    r = packet.getlayer(Raw).load
+    try:
+      p = json.loads(r)
+      print(p)
+    except Exception as e:
+      logging.error(e)
+      pass
 
 sniff(iface="en0", prn=sniffer)
